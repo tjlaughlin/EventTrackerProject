5dcd4d3f1478188f84549221b69b85fdbd5bb571
Tj Laughlin <tlaughl1@gmail.com>
2020-11-13 16:11:02 -0700

    added test for entity

--- JPASci-FiLibrary/src/main/java/com/skilldistillery/library/entities/Novel.java
+++ JPASci-FiLibrary/src/main/java/com/skilldistillery/library/entities/Novel.java
@@ -1,5 +1,78 @@
 package com.skilldistillery.library.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Novel {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Novel() {
+		super();
+	}
+	
+	
+	
+
+	@Override
+	public String toString() {
+		return "Novel [id=" + id + ", title=" + title + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Novel other = (Novel) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public Novel(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	
+	
+	
 }
--- JPASci-FiLibrary/src/test/java/com/skilldistillery/library/entities/NovelTest.java
+++ JPASci-FiLibrary/src/test/java/com/skilldistillery/library/entities/NovelTest.java
@@ -2,13 +2,56 @@ package com.skilldistillery.library.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class NovelTest {
 
+	private static EntityManagerFactory emf;
+	
+	private EntityManager em;
+	
+	private Novel novel;
+	
+	
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("ScienceFictionLibraryPU");
+		
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		novel = em.find(Novel.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		novel = null;
+	}
+
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(novel);
+		assertEquals(1, novel.getId());
+		assertEquals("Dune", novel.getTitle());
+		
 	}
 
 }
